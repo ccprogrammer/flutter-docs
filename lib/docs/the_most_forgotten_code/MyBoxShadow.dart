9c8e748a20cac1bbe63a881d569f9c45cc8cd039
Lubianca Samora <90954993+ccprogrammer@users.noreply.github.com>
2022-07-21 14:41:35 +0700

    Update MyBoxShadow.dart

--- lib/docs/the_most_forgotten_code/MyBoxShadow.dart
+++ lib/docs/the_most_forgotten_code/MyBoxShadow.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyBoxShadow {
-  var gradient = BoxDecoration(
+  var shadow = BoxDecoration(
     boxShadow: [
       BoxShadow(
         color: Colors.black12,
