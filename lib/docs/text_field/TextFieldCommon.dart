966128dc3d3a7bebfa7918b056d5a4ce7244d222
Lubianca Samora <90954993+ccprogrammer@users.noreply.github.com>
2022-11-08 07:54:30 +0700

    Update TextFieldCommon.dart

--- lib/docs/text_field/TextFieldCommon.dart
+++ lib/docs/text_field/TextFieldCommon.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:utara_drive/themes/my_themes.dart';
 
 class CustomTextField extends StatefulWidget {
   const CustomTextField(
@@ -8,12 +9,14 @@ class CustomTextField extends StatefulWidget {
       this.hintText = '',
       required this.onChangeListener,
       this.controller,
-      this.obscureText = false})
+      this.obscureText = false,
+      required this.label})
       : super(key: key);
   final String hintText;
   final Function onChangeListener;
   final TextEditingController? controller;
   final bool obscureText;
+  final String label;
 
   @override
   State<CustomTextField> createState() => _CustomTextFieldState();
@@ -27,42 +30,58 @@ class _CustomTextFieldState extends State<CustomTextField> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.only(top: 32),
-      padding: EdgeInsets.symmetric(horizontal: 16),
-      decoration: BoxDecoration(
-        color: Colors.grey.withOpacity(0.2),
-        borderRadius: BorderRadius.circular(14),
-      ),
-      child: Row(
+      margin: const EdgeInsets.only(top: 17),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Expanded(
-            child: TextFormField(
-              controller: widget.controller,
-              obscureText: widget.obscureText ? isObscure : false,
-              decoration: InputDecoration.collapsed(
-                hintText: widget.hintText,
-              ),
-              onChanged: (value) {
-                if (_debounce?.isActive ?? false) _debounce!.cancel();
-                _debounce = Timer(const Duration(milliseconds: 800), () {
-                  widget.onChangeListener(value);
-                });
-              },
+          Text(
+            widget.label,
+            style: const TextStyle(
+              color: Colors.grey,
+           
             ),
           ),
-          widget.obscureText
-              ? GestureDetector(
-                  onTap: () {
-                    setState(() {
-                      isObscure = !isObscure;
-                    });
-                  },
-                  child: Icon(
-                    isObscure ? Icons.visibility : Icons.visibility_off,
-                    color: Colors.grey,
+          Container(
+            margin: const EdgeInsets.only(top: 5),
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            decoration: BoxDecoration(
+              color: Colors.grey,
+              borderRadius: BorderRadius.circular(5),
+            ),
+            height: 45,
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    controller: widget.controller,
+                    obscureText: widget.obscureText ? isObscure : false,
+                    decoration: InputDecoration.collapsed(
+                      hintText: widget.hintText,
+                    ),
+                    onChanged: (value) {
+                      if (_debounce?.isActive ?? false) _debounce!.cancel();
+                      _debounce = Timer(const Duration(milliseconds: 800), () {
+                        widget.onChangeListener(value);
+                      });
+                    },
                   ),
-                )
-              : SizedBox(),
+                ),
+                widget.obscureText
+                    ? GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isObscure = !isObscure;
+                          });
+                        },
+                        child: Icon(
+                          isObscure ? Icons.visibility : Icons.visibility_off,
+                          color: Colors.grey,
+                        ),
+                      )
+                    : const SizedBox(),
+              ],
+            ),
+          ),
         ],
       ),
     );
