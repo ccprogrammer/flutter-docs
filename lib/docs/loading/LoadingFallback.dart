cbee76975c273250e16697a972973cdf25fbad4b
Lubianca Samora <90954993+ccprogrammer@users.noreply.github.com>
2022-11-09 11:16:19 +0700

    Update LoadingFallback.dart

--- lib/docs/loading/LoadingFallback.dart
+++ lib/docs/loading/LoadingFallback.dart
@@ -1,64 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:loading_overlay/loading_overlay.dart';
-//   loading_overlay: ^0.3.0
 
 class LoadingFallback extends StatelessWidget {
-  LoadingFallback(
+  const LoadingFallback(
       {required this.isLoading,
       required this.child,
       required this.loadingLabel,
-      Key? key})
+      Key? key,
+      required this.context})
       : super(key: key);
 
   final bool isLoading;
   final Widget child;
   final String loadingLabel;
+  final BuildContext context;
 
   @override
   Widget build(BuildContext context) {
+    buildIndicator() {
+      return Container(
+        padding: const EdgeInsets.all(16),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(8.0),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const SizedBox(
+              width: 26,
+              height: 26,
+              child: CircularProgressIndicator(
+                color: Colors.cyan,
+              ),
+            ),
+            const SizedBox(width: 10),
+            DefaultTextStyle(
+              style: const TextStyle(
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.black),
+              child: Text(
+                loadingLabel,
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return LoadingOverlay(
       isLoading: isLoading,
       color: Colors.black87,
-      progressIndicator: LoadingOverlayWidget(label: loadingLabel),
+      progressIndicator: buildIndicator(),
       child: child,
-    );
-  }
-}
-
-class LoadingOverlayWidget extends StatelessWidget {
-  LoadingOverlayWidget({required this.label, Key? key}) : super(key: key);
-
-  final String label;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width / 1.5,
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        border: Border.all(
-          color: Colors.transparent,
-          width: 0.5,
-        ),
-        borderRadius: BorderRadius.all(
-          Radius.circular(8.0),
-        ),
-      ),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation(Colors.red),
-          ),
-          SizedBox(width: 10),
-          Text(
-            label != null && label != '' ? label : 'Loading ...',
-            style: TextStyle(
-                fontSize: 17, fontWeight: FontWeight.w500, color: Colors.black),
-          )
-        ],
-      ),
     );
   }
 }
