abfb7da7888244739eb97d87d93f371ebf770be8
Lubianca Samora <90954993+ccprogrammer@users.noreply.github.com>
2022-11-09 07:13:21 +0700

    Update AlertDialogCommon2.dart

--- lib/docs/alert_dialog/AlertDialogCommon2.dart
+++ lib/docs/alert_dialog/AlertDialogCommon2.dart
@@ -1,63 +1,67 @@
 import 'package:flutter/material.dart';
 
-Future AlertDialogCommon2({
-  required BuildContext context,
-  text = '',
-  icon,
-  onClose,
-}) {
-  return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(
-                width: 54,
-                height: 54,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Color(0xff1C1B1F), width: 3),
-                  shape: BoxShape.circle,
-                ),
-                child: Icon(
+Future showAlertDialog({
+    required BuildContext context,
+    text = 'dialog label',
+    icon = Icons.warning,
+    String titleLeft = 'No',
+    String titleRight = 'Yes',
+    Function? onClose,
+    Function? onYes,
+    Function? onNo,
+  }) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Icon(
                   icon,
-                  size: 32,
+                  size: 48,
+                  color: MyTheme.colorRed,
                 ),
-              ),
-              SizedBox(height: 17),
-              Container(
-                child: Text(
+                const SizedBox(height: 24),
+                Text(
                   text,
                   style: const TextStyle(),
                 ),
-              ),
-              InkWell(
-                onTap: () async {
-                  Navigator.pop(context);
-                  onClose();
-                },
-                child: Container(
-                  alignment: Alignment.center,
-                  decoration: const BoxDecoration(
-                      color: Color(0xff1C1B1F),
-                      borderRadius: BorderRadius.all(Radius.circular(8))),
-                  padding: const EdgeInsets.fromLTRB(25, 10, 25, 10),
-                  margin: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                  child: const Text(
-                    "Tutup",
-                    style: TextStyle(
-                      fontSize: 14,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          onNo != null ? onNo() : null;
+                          Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: MyTheme.colorCyan,
+                        ),
+                        child: Text(titleLeft),
+                      ),
                     ),
-                  ),
+                    const SizedBox(width: 24),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          onYes != null ? onYes() : null;
+                          Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: MyTheme.colorRed,
+                        ),
+                        child: Text(titleRight),
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
-        );
-      }).then((value) => onClose());
-}
+              ],
+            ),
+          );
+        }).then((value) => onClose != null ? onClose() : null);
+  }
